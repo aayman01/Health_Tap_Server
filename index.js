b9d184d563187e21620617210b49a2aad1db488a
aayman01 <ayman.alfi098@gmail.com>
2024-05-13 02:16:40 +0600

    getting service provider data by id

--- index.js
+++ index.js
@@ -73,6 +73,15 @@ async function run() {
       res.send(result)
     })
 
+    // getting serive provider data by id
+
+    app.get("/allServiceProvider/:id",async(req, res)=> {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await serviceProviderCollection.findOne(query).toArray();
+      res.send(result)
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
