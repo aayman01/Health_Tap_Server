f260698ba7a35b11fc5ef3f01c28372562d91431
aayman01 <ayman.alfi098@gmail.com>
2024-05-13 14:46:06 +0600

    getting all booked data by email

--- index.js
+++ index.js
@@ -120,6 +120,16 @@ async function run() {
       res.send(result)
     });
 
+
+    // getting all booked serive data
+    
+    app.get("/bookedService/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { userEmail: email };
+      const result = await bookedServiceCollection.find(query).toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
